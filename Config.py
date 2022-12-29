3d79a3e81271b37e947358c84f94de851d96b445
Void-Great-Emperor <akshaykumarsb177013@gmail.com>
2022-12-30 02:48:53 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,7 +4,7 @@ class Config(object):
     API_ID = int(os.environ.get("APP_ID", "21927988"))
     API_HASH = os.environ.get("API_HASH", "e18f720acdff1e5b0ec80616aecd8a5a")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5879384212:AAFHPL8fpCP6cl1L5jA4xHmX4Sz-S_fzivM")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOGsBu2mzr3A9aH-a5aJPD5kIP34gH8WAUWkYE53JatwXtYNA1vhOmY_yeB8ez7xopkWs_Nu0mdr7VIw1Q6-nWwTN5BT7_d7CyQCFi7-sZKvTIoy21lEbJS7USFrdX_EAwVlcK4UVbaumPzZxVZr7jLR7CxzASaoh5hLUvvv4_RWaBPJUQR_SdwB9olqYfjlZASV1KQmpA918OAvf2s7Vuzvfi8N9LhkOViCNIzhrFzNTMOWBXe7e5AD8CqsKKJjYsdfGNybzB9HNuUcWiAoKopXw_OIm5pK2IKpjBm-ceJfovu8rhGHJwT801SbIW95Ywqe6uh7kBJCxcxYktXiF6cC9txQ=")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOGsBu0mCeCecmJlIDPbT-0xEXrboJLLzvkoc54eGCMrxc-i6zng_TP7J-bQCa_lihghpL9gHI5nL9o-ws57m_0NTLA-oWlpY5yFM7ZTE6h6IjAXyTtzOXaHlMZW3vThrLoEnv3aGTuix5eljBTVtBiYdncQhoI0QT7DEOHhVIY35aDZWnNRedv672erBeHB8OdggHPuzgiEL996Ng_QLffzquW2il1ORfguGXeNWkJEQXq0MjSgG4HrYKP5EwG_C_cuV0hVgyZIV58ZLgiCnqwhLCIntdAGnKnTkEd5Dj-EkjQjD4QzBxJpbZH5hRRu1lLdeBFvypn7nnhE0mLfZ790Ji38=")
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "Shuna_utabot")
